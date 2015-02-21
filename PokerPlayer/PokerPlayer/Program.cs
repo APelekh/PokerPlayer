5393fab60631046baffc9256ac4d7958b61a4ffe
Andrii Pelekh <pelekhav@gmail.com>
2015-02-21 15:42:23 -0700

    Changes 2-21-15

--- PokerPlayer/PokerPlayer/Program.cs
+++ PokerPlayer/PokerPlayer/Program.cs
@@ -33,7 +33,9 @@ namespace PokerPlayer
     class PokerPlayer
     {
         //declaring a list to store the player's hand
-        private List<Card> Hand = new List<Card>();
+        private List<Card> _Hand = new List<Card>();
+        private List<Card> Hand { get; set; }
+        
         
         //FOR TESTING
         //static Card card1 = new Card(8, 1);
@@ -53,7 +55,7 @@ namespace PokerPlayer
         /// <param name="list">List of cards</param>
         public void DrawHand (List<Card> list)
         {
-            Hand = list;
+            this.Hand = list;
         }
         
         // Enum of different hand types
@@ -76,48 +78,62 @@ namespace PokerPlayer
         /// </summary>
         public HandType HandRank
         {
-            get { 
-                if (this.HasPair() == true)
-                {
-                    return HandType.OnePair; 
-                }
-                else if (this.HasTwoPair() == true )
-                {
-                    return HandType.TwoPair;
-                }
-                else if (this.HasThreeOfAKind() == true)
-                {
-                    return HandType.ThreeOfAKind;
-                }
-                else if (this.HasStraight() == true)
-                {
-                    return HandType.Straight;
-                }
-                else if (this.HasFlush() == true)
-                {
-                    return HandType.Flush;
-                }
-                else if (this.HasFullHouse() == true)
-                {
-                    return HandType.FullHouse;
-                }
-                else if (this.HasFourOfAKind() == true)
-                {
-                    return HandType.FourOfAKind;
-                }
-                else if (this.HasStraightFlush() == true)
-                {
-                    return HandType.StraightFlush;
-                }
-                else if (this.HasRoyalFlush() == true)
-                {
-                    return HandType.RoyalFlush;
-                }
-                else
-                {
-                    return HandType.HighCard;
-                }
+            get
+            {
+                if (HasRoyalFlush()) { return HandType.RoyalFlush; }
+                if (HasStraightFlush()) { return HandType.StraightFlush; }
+                if (HasFourOfAKind()) { return HandType.FourOfAKind; }
+                if (HasFullHouse()) { return HandType.FullHouse; }
+                if (HasFlush()) { return HandType.Flush; }
+                if (HasStraight()) { return HandType.Straight; }
+                if (HasThreeOfAKind()) { return HandType.ThreeOfAKind; }
+                if (HasTwoPair()) { return HandType.TwoPair; }
+                if (HasPair()) { return HandType.OnePair; }
+                return HandType.HighCard;
             }
+            
+            //get { 
+            //    if (this.HasPair())
+            //    {
+            //        return HandType.OnePair; 
+            //    }
+            //    else if (this.HasTwoPair())
+            //    {
+            //        return HandType.TwoPair;
+            //    }
+            //    else if (this.HasThreeOfAKind())
+            //    {
+            //        return HandType.ThreeOfAKind;
+            //    }
+            //    else if (this.HasStraight())
+            //    {
+            //        return HandType.Straight;
+            //    }
+            //    else if (this.HasFlush())
+            //    {
+            //        return HandType.Flush;
+            //    }
+            //    else if (this.HasFullHouse())
+            //    {
+            //        return HandType.FullHouse;
+            //    }
+            //    else if (this.HasFourOfAKind())
+            //    {
+            //        return HandType.FourOfAKind;
+            //    }
+            //    else if (this.HasStraightFlush())
+            //    {
+            //        return HandType.StraightFlush;
+            //    }
+            //    else if (this.HasRoyalFlush())
+            //    {
+            //        return HandType.RoyalFlush;
+            //    }
+            //    else
+            //    {
+            //        return HandType.HighCard;
+            //    }
+            //}
         }
 
         // Constructor that isn't used
@@ -131,11 +147,13 @@ namespace PokerPlayer
         {
             //List<IGrouping<Rank, Card>> handToCheck = Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).ToList();
             //if (handToCheck.Any(x => x.Count() == 2 && x.Count() <= 2))
-            if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 1 && HasFullHouse() == false)
-            {
-                return true;
-            }
-            return false;
+            //if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 1 && HasFullHouse() == false)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            return this.Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 1;
         }
         /// <summary>
         /// Checking for Two Pairs combination
@@ -144,11 +162,13 @@ namespace PokerPlayer
         public bool HasTwoPair()
         {
             //List<IGrouping<Rank, Card>> handToCheck = Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).ToList();
-            if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 2)
-            {
-                return true;
-            }
-            return false;
+            //if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 2)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            return this.Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 2;
         }
         /// <summary>
         /// Checking for Three of a Kind combination
@@ -156,12 +176,15 @@ namespace PokerPlayer
         /// <returns>True if combination is valid and false if not<</returns>
         public bool HasThreeOfAKind()
         {
-            //List<IGrouping<Rank, Card>> handToCheck = Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).ToList();
-            if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).Count() == 1 && HasFullHouse() == false)
-            {
-                return true;
-            }
-            return false;
+            ////List<IGrouping<Rank, Card>> handToCheck = Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).ToList();
+            //if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).Count() == 1 && HasFullHouse() == false)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            return this.Hand.GroupBy(x => x.Rank).Any(x => x.Count() == 3);
+            //return this.Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).Count() == 1;
         }
         /// <summary>
         /// Checking for Straight combination
@@ -169,13 +192,33 @@ namespace PokerPlayer
         /// <returns>True if combination is valid and false if not<</returns>
         public bool HasStraight()
         {
-            List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
-            Rank rankOfFirstCard = handToCheck[0].Rank;
-            if ((rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3 && rankOfFirstCard == handToCheck[4].Rank - 4) && HasStraightFlush() == false && HasRoyalFlush() == false)
+            //List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
+            //Rank rankOfFirstCard = handToCheck[0].Rank;
+            //if ((rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3 && rankOfFirstCard == handToCheck[4].Rank - 4) && HasStraightFlush() == false && HasRoyalFlush() == false)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            // Make sure all of the cards are distinct
+            if (!HasPair())
             {
-                return true;
-            }
-            return false;
+                // Ace is used as the low card (has Ace and 2), check for straight
+                if (this.Hand.OrderBy(x => x.Rank).Last().Rank == Rank.Ace)
+                {
+                    List<Card> tempHand = Hand.OrderBy(x => x.Rank).ToList();
+                    // Remove the Ace
+                    tempHand.RemoveAt(tempHand.Count - 1);
+                    // Then check for the straight with 2,3,4,5
+                    return (tempHand.OrderBy(x => x.Rank).Last().Rank - tempHand.OrderBy(x => x.Rank).First().Rank) == 3;
+                }
+                else
+                {
+                    // Check for normal straight
+                    return (this.Hand.OrderBy(x => x.Rank).Last().Rank - this.Hand.OrderBy(x => x.Rank).First().Rank) == 4;
+                }
+            } // If there are any duplicate cards, then it can't be a straight
+            else { return false; }
         }
         /// <summary>
         /// Checking for Flush combination
@@ -183,13 +226,16 @@ namespace PokerPlayer
         /// <returns>True if combination is valid and false if not<</returns>
         public bool HasFlush()
         {
-            List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
-            Suit suitOfFirstCard = handToCheck[0].Suit;
-            if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && HasStraightFlush() == false && HasRoyalFlush() == false)
-            {
-                return true;
-            }
-            return false;
+            //List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
+            //Suit suitOfFirstCard = handToCheck[0].Suit;
+            //if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && HasStraightFlush() == false && HasRoyalFlush() == false)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            return this.Hand.GroupBy(x => x.Suit).Count() == 1;
+            //return this.Hand.Select(x => x.Suit).Distinct().Count() == 1;
         }
         /// <summary>
         /// Checking for Full House combination
@@ -203,11 +249,13 @@ namespace PokerPlayer
             //}
             //List<IGrouping<Rank, Card>> handToCheckOne = Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).ToList();
             //List<IGrouping<Rank, Card>> handToCheckTwo = Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).ToList();
-            if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 1 && Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).ToList().Count() == 1)
-            {
-                return true;
-            }
-            return false;
+            //if (Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 1 && Hand.GroupBy(x => x.Rank).Where(x => x.Count() == 3).ToList().Count() == 1)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+            return HasThreeOfAKind() && HasPair();
         }
         /// <summary>
         /// Checking for Four of a Kind combination
@@ -227,6 +275,10 @@ namespace PokerPlayer
                 return true;
             }
             return false;
+
+            //return this.Hand.GroupBy(x => x.Rank).Where(grouping => grouping.Count() == 4).Count() == 1 &&
+            //    this.Hand.GroupBy(x => x.Rank).OrderBy(x => x.Count()).Last().Select(x => x.Suit).Distinct().Count() == 4;
+            //after the &&, we are selecting the cards in the 4 of a kind, and then ensuring that they have different suits.
         }
         /// <summary>
         /// Checking for Straight Flush combination
@@ -234,25 +286,26 @@ namespace PokerPlayer
         /// <returns>True if combination is valid and false if not<</returns>
         public bool HasStraightFlush()
         {
-            List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
-            Suit suitOfFirstCard = handToCheck[0].Suit;
-            Rank rankOfFirstCard = handToCheck[0].Rank;
-            //if (HasRoyalFlush() == true)
+            //List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
+            //Suit suitOfFirstCard = handToCheck[0].Suit;
+            //Rank rankOfFirstCard = handToCheck[0].Rank;
+            ////if (HasRoyalFlush() == true)
+            ////{
+            ////    return false;
+            ////}
+            //if (handToCheck[0].Rank == Rank.Two && handToCheck[4].Rank == Rank.Ace && HasRoyalFlush() == false)
             //{
-            //    return false;
+            //    if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && (rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3))
+            //    {
+            //        return true;
+            //    }
             //}
-            if (handToCheck[0].Rank == Rank.Two && handToCheck[4].Rank == Rank.Ace && HasRoyalFlush() == false)
-            {
-                if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && (rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3))
-                {
-                    return true;
-                }
-            }
-            else if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && (rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3 && rankOfFirstCard == handToCheck[4].Rank - 4) && HasRoyalFlush() == false)
-            {
-                return true;
-            }
-            return false;
+            //else if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && (rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3 && rankOfFirstCard == handToCheck[4].Rank - 4) && HasRoyalFlush() == false)
+            //{
+            //    return true;
+            //}
+            //return false;
+            return HasStraight() && HasFlush();
         }
         /// <summary>
         /// Checking for Royal Flush combination
@@ -260,17 +313,24 @@ namespace PokerPlayer
         /// <returns>True if combination is valid and false if not<</returns>
         public bool HasRoyalFlush()
         {
-            List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
-            Suit suitOfFirstCard = handToCheck[0].Suit;
-            Rank rankOfFirstCard = handToCheck[0].Rank;
-            if (rankOfFirstCard == Rank.Ten)
+            //List<Card> handToCheck = Hand.OrderBy(x => x.Rank).ToList();
+            //Suit suitOfFirstCard = handToCheck[0].Suit;
+            //Rank rankOfFirstCard = handToCheck[0].Rank;
+            //if (rankOfFirstCard == Rank.Ten)
+            //{
+            //    if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && (rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3 && rankOfFirstCard == handToCheck[4].Rank - 4))
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+
+            // Check that there is a Ten and an Ace
+            if ((this.Hand.Where(x => x.Rank == Rank.Ten).Count() == 1) && (this.Hand.Where(x => x.Rank == Rank.Ace).Count() == 1))
             {
-                if ((suitOfFirstCard == handToCheck[1].Suit && suitOfFirstCard == handToCheck[2].Suit && suitOfFirstCard == handToCheck[3].Suit && suitOfFirstCard == handToCheck[4].Suit) && (rankOfFirstCard == handToCheck[1].Rank - 1 && rankOfFirstCard == handToCheck[2].Rank - 2 && rankOfFirstCard == handToCheck[3].Rank - 3 && rankOfFirstCard == handToCheck[4].Rank - 4))
-                {
-                    return true;
-                }
-            }
-            return false;
+                return HasStraightFlush();
+            } // if those cards aren't in the hand it can't be a royal flush
+            else { return false; }
         }
     }
     //Guides to pasting your Deck and Card class
